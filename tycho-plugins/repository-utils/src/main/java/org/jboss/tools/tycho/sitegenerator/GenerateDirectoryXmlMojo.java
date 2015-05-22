ead70c84d5030d2b323647acdef3c4f783d69235
nickboldt <nickboldt@gmail.com>
2015-05-22 14:43:20 -0400

    remove unused import

--- tycho-plugins/repository-utils/src/main/java/org/jboss/tools/tycho/sitegenerator/GenerateDirectoryXmlMojo.java
+++ tycho-plugins/repository-utils/src/main/java/org/jboss/tools/tycho/sitegenerator/GenerateDirectoryXmlMojo.java
@@ -20,7 +20,6 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.eclipse.tycho.ArtifactKey;
 import org.eclipse.tycho.PackagingType;
 
 @Mojo(defaultPhase = LifecyclePhase.PACKAGE, name = "generate-discovery-site")
